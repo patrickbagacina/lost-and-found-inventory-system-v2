37e3f135f625cc53155be7a10711e4d0eb6f978a
Patrick Bagacina <bagacinapatrick.jpb@gmail.com>
2023-09-30 12:29:40 +0000

    Setup docker env for database

---
+++ postgres-db/create-tables.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA IF NOT EXISTS laf;
+
+CREATE TABLE IF NOT EXISTS laf.user (
+  user_id SERIAL PRIMARY KEY,
+  name varchar(250) NOT NULL,
+  username varchar(250) NOT NULL,
+  password varchar(250) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS laf.lost_item (
+  item_id SERIAL PRIMARY KEY,
+  category varchar(50) NOT NULL,
+  type varchar(50) NOT NULL,
+  brand varchar(50) NOT NULL,
+  description text NOT NULL,
+  is_redeemed boolean NOT NULL,
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
