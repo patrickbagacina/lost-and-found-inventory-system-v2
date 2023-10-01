d12d49ac5454bd135a1af826a30b88a31497c18d
Patrick Bagacina <bagacinapatrick.jpb@gmail.com>
2023-10-01 10:45:00 +0000

    Setup docker env for the nest server.

--- postgres-db/create-tables.sql
+++ postgres-db/create-tables.sql
@@ -12,10 +12,10 @@ CREATE TABLE IF NOT EXISTS laf.user (
 CREATE TABLE IF NOT EXISTS laf.lost_item (
   item_id SERIAL PRIMARY KEY,
   category varchar(50) NOT NULL,
-  type varchar(50) NOT NULL,
+  item_type varchar(50) NOT NULL,
   brand varchar(50) NOT NULL,
   description text NOT NULL,
-  is_redeemed boolean NOT NULL,
+  is_claimed boolean NOT NULL,
   created_at timestamp NOT NULL DEFAULT NOW(),
   updated_at timestamp NOT NULL DEFAULT NOW()
 );
